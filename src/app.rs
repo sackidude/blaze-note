657dc32ca23961c0285d0b18d2734240326a2154
sackidude <nils.nilsen9@outlook.com>
2024-04-23 21:44:52 +0200

    Remove template stuff

--- src/app.rs
+++ src/app.rs
@@ -39,12 +39,7 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1>"Blaze Note"</h1>
     }
 }
