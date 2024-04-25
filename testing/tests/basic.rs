44dfdc589a06d9de48589917421fd4366cd6b22c
sackidude <nils.nilsen9@outlook.com>
2024-04-25 16:47:34 +0200

    wrong path

--- testing/tests/basic.rs
+++ testing/tests/basic.rs
@@ -2,9 +2,9 @@ use std::fs::read_to_string;
 
 #[test]
 fn main() {
-    let hello_str: String = read_to_string("../documents/basic.md")
+    let hello_str: String = read_to_string("documents/basic.md")
         .unwrap()
         .parse()
         .unwrap();
-    let hello = blaze_note_parser::parse(&hello_str).expect("Failed to parse note: {}");
+    let _hello = blaze_note_parser::parse(&hello_str).unwrap();
 }
