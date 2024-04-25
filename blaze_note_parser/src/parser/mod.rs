8729150637dcffb8bf2cae0e9985b3f665707d81
sackidude <nils.nilsen9@outlook.com>
2024-04-25 19:00:23 +0200

    beginning of parser

--- blaze_note_parser/src/parser/mod.rs
+++ blaze_note_parser/src/parser/mod.rs
@@ -1,6 +1,44 @@
-use crate::{error::Result, flashcard::Flashcard};
+use crate::{
+    error::{Error, Result},
+    flashcard::Flashcard,
+};
 
 pub fn parse_to_cards(document: &str) -> Result<Vec<Flashcard>> {
-    let cards = vec![];
+    // First seperate the document into all of the individual cards and then
+    // parse them.
+    let mut last_char: char = '\0';
+    let mut in_card = false;
+    for c in document.chars() {
+        match c {
+            '{' => {
+                // This is a new opening to a card and your aren't already in
+                // a card.
+                if last_char == '{' && !in_card {
+                    in_card = true;
+                }
+            }
+            '}' => {
+                if last_char == '}' && in_card {
+                    if in_card {
+                        in_card = false;
+                    } else {
+                        // TODO!: Think about this more.
+                        return Err(Error::InvalidNoteSyntax);
+                    }
+                }
+            }
+            _ => (),
+        }
+        last_char = c;
+    }
+
+    let cards: Vec<Flashcard> = vec![];
+
     Ok(cards)
 }
+
+#[test]
+fn basic() {}
+
+#[test]
+fn utf8() {}
