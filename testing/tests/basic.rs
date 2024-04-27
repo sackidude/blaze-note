1b6bf1b50fa0c0ca91f42e6029c424617a78ef2f
sackidude <nils.nilsen9@outlook.com>
2024-04-27 16:03:02 +0200

    expanded test

--- testing/tests/basic.rs
+++ testing/tests/basic.rs
@@ -1,10 +1,19 @@
-use blaze_note_parser::parse_flashcards;
+use blaze_note_parser::{
+    flashcard::{Flashcard, FrontBack},
+    parse_flashcards,
+};
 
 #[test]
 fn basic_card_parsing() {
     let note_str = String::from("# {{Hello,| World!}}");
     let flashcards = parse_flashcards(&note_str).expect("failed to parse document");
     assert_eq!(flashcards.len(), 1);
-
+    let card = &flashcards[0];
+    if let Flashcard::FrontBack(card) = card {
+        assert_eq!(card.front(), "Hello,");
+        assert_eq!(card.back(), " World!");
+    } else {
+        panic!("incorrect card type");
+    }
     //TODO! Add more here
 }
