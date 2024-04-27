faae18d6f486d3ca8ccb3c16231e12a2f3f5fc04
sackidude <nils.nilsen9@outlook.com>
2024-04-27 15:26:41 +0200

    basic builder pattern

--- blaze_note_parser/src/flashcard.rs
+++ blaze_note_parser/src/flashcard.rs
@@ -20,17 +20,44 @@ pub struct List {
 // I don't love derive Clone and Copy, but removing it leads to a big fight with
 // the borrow checker that I don't feel like doing right now
 #[derive(Clone, Copy)]
-pub(crate) struct FlashcardBuilder {}
+pub(crate) struct FlashcardBuilder {
+    card_type: Option<FlashcardTypes>,
+}
+
+#[derive(Clone, Copy)]
+pub(crate) enum FlashcardTypes {
+    FrontBack,
+    Reveal,
+    OrderedList,
+}
 
 impl FlashcardBuilder {
     pub fn new() -> Self {
-        FlashcardBuilder {}
+        FlashcardBuilder { card_type: None }
     }
 
-    pub fn build(self) -> Flashcard {
-        Flashcard::FrontBack(FrontBack {
-            front: "hello".into(),
-            back: "world".into(),
-        })
+    pub fn build(self) -> crate::error::Result<Flashcard> {
+        if let Some(card_type) = self.card_type {
+            return Ok(match card_type {
+                FlashcardTypes::FrontBack => Flashcard::FrontBack(FrontBack {
+                    front: "Hello".into(),
+                    back: "World".into(),
+                }),
+                FlashcardTypes::Reveal => Flashcard::Reveal(Reveal {
+                    wrapper: "Hello".into(),
+                    reveal: "World".into(),
+                }),
+                FlashcardTypes::OrderedList => Flashcard::OrderedList(List {
+                    question: "Hello".into(),
+                    entries: vec!["World".into()],
+                }),
+            });
+        }
+        Err(crate::error::Error::EmptyCard)
+    }
+
+    pub fn card_type(mut self, card_type: FlashcardTypes) -> Self {
+        self.card_type = Some(card_type);
+        self
     }
 }
