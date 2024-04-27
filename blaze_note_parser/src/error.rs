501a78816837ce4bc2b704eb274a38b7c6b4f4fc
sackidude <nils.nilsen9@outlook.com>
2024-04-27 15:26:48 +0200

    new error

--- blaze_note_parser/src/error.rs
+++ blaze_note_parser/src/error.rs
@@ -10,12 +10,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     UnclosedBrackets,
+    EmptyCard,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::UnclosedBrackets => write!(f, "unclosed brackets at end of document"),
+            Error::EmptyCard => write!(f, "card without any of the card indicators '|' '||' '|||'"),
         }
     }
 }
